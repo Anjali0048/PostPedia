3729df8260d84184f624c65120ec24ef9e181009
Anjali Singh <105203288+Anjali0048@users.noreply.github.com>
2023-05-26 19:12:04 +0530

    commit all

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          PostPedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
